3ec0c29f00d83c111b238a6329515c4fdbadee23
Cory <152090557+Cory545@users.noreply.github.com>
2024-06-12 09:14:45 +0100

    Add files via upload

---
+++ 15.1 Love Chance Generator/script.js
@@ -0,0 +1,9 @@
+
+let nameOne = prompt("What is the name?");
+let nameTwo = prompt("What is the name of the other?");
+
+var loveScore = Math.random();
+loveScore = loveScore * 100;
+loveScore = Math.floor(loveScore);
+
+alert(nameOne+" and "+nameTwo+" have a "+loveScore+"% chance at being the One for each other forever.");
