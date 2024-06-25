ba5185dab8cec8cf9c772922cf3ac7ec95c9ac8d
Cory <152090557+Cory545@users.noreply.github.com>
2024-06-25 20:39:04 +0100

    Add files via upload

---
+++ 23.3 NPM/index.js
@@ -0,0 +1,10 @@
+
+import generateName from "sillyname";
+let sillyName = generateName();
+
+console.log("My name is " + sillyName + ".");
+
+import {randomSuperhero} from 'superheroes';
+ 
+const superHeroName = randomSuperhero();
+console.log("I am " + superHeroName + "!");
